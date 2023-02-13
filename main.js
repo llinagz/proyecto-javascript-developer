5bd9402a2957995cdb211c7ad46ee37280d3dfa1
Javi <javierllinaresbarrral@outlook.com>
2023-02-13 23:28:43 +0100

    Agregar estilos , interaccion y menu para la version movil

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const emailNavBar = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 
+const mobileMenu = document.querySelector(".mobile-menu");
+const hamburguerMenuMobile = document.querySelector(".menu");
+
 emailNavBar.addEventListener("click", toggleMenu);
+hamburguerMenuMobile.addEventListener("click", toggleMenuMobile)
 
 function toggleMenu(){
     // if(!desktopMenu.classList.contains("inactive")){
@@ -12,4 +16,8 @@ function toggleMenu(){
 
     //Mejor práctica
     desktopMenu.classList.toggle("inactive");
+}
+
+function toggleMenuMobile(){
+    mobileMenu.classList.toggle("inactive");
 }
